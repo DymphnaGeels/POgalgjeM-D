7af4851b3995a338225fcf36a375e77c7c7b050d
Dymphna Geels <110533@st-maartenscollege.nl>
2020-07-03 09:41:10 +0000

    auto-format.

--- main.py
+++ main.py
@@ -1,81 +1,87 @@
 import random
 from words import word_list
- 
+
 
 def get_word():
-   word = random.choice(word_list)
-   return word.upper()
+    word = random.choice(word_list)
+    return word.upper()
 
 
 def play(word):
-   word_completion = "_" * len(word)
-   guessed = False
-   guessed_letters = []
-   guessed_words = []
-   tries = 5
-   print("Laten we galgje spelen!")
-   print('De regels:')
-   lijst = ['-na een ingevoerde letter op enter drukken.', '-na vijf foute antwoorden ben je af.']
-   for item in lijst:
-     print(item)
-   print(display_hangman(tries))
-   print(word_completion)
-   print("\n")
-   print('het woord heeft', len(word), 'letters') 
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 5
+    print("Laten we galgje spelen!")
+    print('De regels:')
+    lijst = [
+        '-na een ingevoerde letter op enter drukken.',
+        '-na vijf foute antwoorden ben je af.'
+    ]
+    for item in lijst:
+        print(item)
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
+    print('het woord heeft', len(word), 'letters')
 
-   if tries == 5:
-    print('je hebt nog 5 beurten over')
+    if tries == 5:
+        print('je hebt nog 5 beurten over')
 
-   while not guessed and tries > 0:
-       guess = input("Typ een letter of een woord:").upper()
-       if len(guess) == 1 and guess.isalpha():
-           if guess in guessed_letters:
-               print("Deze letter heb je al geraden.", guess)
-           elif guess not in word:
-               print(guess, "zit niet in het woord.")
-               tries -= 1
-               print('je verliest een beurt, nog', (tries),'beurten over')
-               print('je hebt de letters', guessed_letters, 'al geprobeerd')
-               guessed_letters.append(guess)
-           else:
-               print("Goed bezig,", guess, "zit in het woord!")
-               guessed_letters.append(guess)
-               word_as_list = list(word_completion)
-               indices = [i for i, letter in enumerate(word) if letter == guess]
-               for index in indices:
-                   word_as_list[index] = guess
-               word_completion = "".join(word_as_list)
-               if "_" not in word_completion:
-                   guessed = True
-       elif len(guess) == len(word) and guess.isalpha():
-           if guess in guessed_words:
-               print("Je hebt het woord al geraden", guess)
-           elif guess != word:
-               print(guess, "is niet het woord.")
-               tries -= 1
-               print('je verliest een beurt, nog', (tries),'beurten over')
-               guessed_words.append(guess)
-               used_letters.append(guess)
-           else:
-               guessed = True
-               word_completion = word
-       else:
-           print("Geen geldige invoer.")
-           tries -= 1
-           print('je verliest een beurt, nog', (tries),'beurten over')
- 
-       print(display_hangman(tries))
-       print(word_completion)
-       print("\n")
-   if guessed:
-       print("Gefeliciteerd, je hebt het woord geraden! je wint!")
-   else:
-       print("Sorry, je hebt geen beurten meer. Het woord was " + word + ". Volgende keer beter!")
+    while not guessed and tries > 0:
+        guess = input("Typ een letter of een woord:").upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("Deze letter heb je al geraden.", guess)
+            elif guess not in word:
+                print(guess, "zit niet in het woord.")
+                tries -= 1
+                print('je verliest een beurt, nog', (tries), 'beurten over')
+                print('je hebt de letters', guessed_letters, 'al geprobeerd')
+                guessed_letters.append(guess)
+            else:
+                print("Goed bezig,", guess, "zit in het woord!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [
+                    i for i, letter in enumerate(word) if letter == guess
+                ]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("Je hebt het woord al geraden", guess)
+            elif guess != word:
+                print(guess, "is niet het woord.")
+                tries -= 1
+                print('je verliest een beurt, nog', (tries), 'beurten over')
+                guessed_words.append(guess)
+                used_letters.append(guess)
+            else:
+                guessed = True
+                word_completion = word
+        else:
+            print("Geen geldige invoer.")
+            tries -= 1
+            print('je verliest een beurt, nog', (tries), 'beurten over')
+
+        print(display_hangman(tries))
+        print(word_completion)
+        print("\n")
+    if guessed:
+        print("Gefeliciteerd, je hebt het woord geraden! je wint!")
+    else:
+        print("Sorry, je hebt geen beurten meer. Het woord was " + word +
+              ". Volgende keer beter!")
 
 
 def display_hangman(tries):
-  stages = [
-              """
+    stages = [
+        """
                  --------
                  |      |
                  |      O
@@ -83,9 +89,7 @@ def display_hangman(tries):
                  |      |
                  |     / \\
                  -
-              """,
- 
-              """
+              """, """
                  --------
                  |      |
                  |      O
@@ -93,9 +97,7 @@ def display_hangman(tries):
                  |      |
                  |     /
                  -
-              """,
- 
-              """
+              """, """
                  --------
                  |      |
                  |      O
@@ -103,9 +105,7 @@ def display_hangman(tries):
                  |      |
                  |    
                  -
-              """,
-  
-              """
+              """, """
                  --------
                  |      |
                  |      O
@@ -113,9 +113,7 @@ def display_hangman(tries):
                  |      |
                  |   
                  -
-              """,
- 
-              """
+              """, """
                  --------
                  |      |
                  |      O
@@ -123,9 +121,7 @@ def display_hangman(tries):
                  |      |
                  |   
                  -
-              """,
-      
-              """
+              """, """
                  --------
                  |      |
                  |      O
@@ -134,17 +130,17 @@ def display_hangman(tries):
                  |   
                  -
               """
-  ]
-  return stages[tries]
+    ]
+    return stages[tries]
 
 
 def main():
-   word = get_word()
-   play(word)
-   while input("Nog een keer spelen? (J/N) ").upper() == "J":
-       word = get_word()
-       play(word)
- 
- 
+    word = get_word()
+    play(word)
+    while input("Nog een keer spelen? (J/N) ").upper() == "J":
+        word = get_word()
+        play(word)
+
+
 if __name__ == "__main__":
-   main()
+    main()
